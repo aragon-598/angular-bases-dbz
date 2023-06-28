2ee41da5729ab7b22a36fda379e415359e212d93
Alejandro Aragon <aragondru155@gmail.com>
2023-06-27 22:01:27 -0600

    changing methods

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -19,4 +19,12 @@ export class HeroeComponent {
     return `${this.name} - ${this.age}`;
   }
 
+  changeName():void{
+    this.name = 'Spiderman'//this.name=='Spiderman'? this.name='Ironman':this.name = 'Spiderman';
+  }
+
+  changeAge():void{
+    this.age = 25//this.age==49? this.age=22:this.age = 49;
+  }
+
 }
